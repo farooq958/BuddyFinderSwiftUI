44c7105dbec5506eebefdcf0d4982240172d9d27
Farooq Khan <farooqafridi958@gmail.com>
2025-12-28 20:28:42 +0500

    Initial Commit

---
+++ BuddyFinder/BuddyFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuddyFinderApp.swift
+//  BuddyFinder
+//
+//  Created by apple on 28/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct BuddyFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuddyFinderTests/BuddyFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  BuddyFinderTests.swift
+//  BuddyFinderTests
+//
+//  Created by apple on 28/12/2025.
+//
+
+import Testing
+@testable import BuddyFinder
+
+struct BuddyFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
